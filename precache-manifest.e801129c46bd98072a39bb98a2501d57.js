ce81bdd6f05fcf44f04bd183f47dd155a5c17389
FriendlyUser <FriendlyUser@users.noreply.github.com>
2020-10-06 01:04:13 +0000

    deploy: bceba0ebb986ea423e372a17fe01741b7834fa11

---
+++ precache-manifest.e801129c46bd98072a39bb98a2501d57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56681eba67fa4164a966",
+    "url": "/css/app.83860a8d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "19e0f9bf08ad5104437ed446f422b9f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "575c5624f4b6d2057d4b",
+    "url": "/js/about.b91d8310.js"
+  },
+  {
+    "revision": "56681eba67fa4164a966",
+    "url": "/js/app.f3ec07b2.js"
+  },
+  {
+    "revision": "84d592948c4662852536",
+    "url": "/js/chunk-vendors.386858f6.js"
+  },
+  {
+    "revision": "db31bf4807f47c668280a84a26a5bfcf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
